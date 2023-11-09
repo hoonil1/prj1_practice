f043723ef7a62974b5bee3fa0f5772a63d8ac067
hoonil1 <phi0588@naver.com>
2023-11-09 16:18:23 +0900

    Spinner 를 테이블 밖으로 뺌

--- src/page/BoardList.js
+++ src/page/BoardList.js
@@ -1,15 +1,31 @@
 import React, { useEffect, useState } from "react";
-import { Box, Spinner, Table, Td, Th, Thead, Tr } from "@chakra-ui/react";
+import {
+  Box,
+  Spinner,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export function BoardList() {
   const [boardList, setBoardList] = useState(null);
 
+  const navigate = useNavigate();
+
   useEffect(() => {
     axios
-      .get('"/api/board/list')
+      .get("/api/board/list")
       .then((response) => setBoardList(response.data));
   }, []);
+
+  if (boardList === null) {
+    return <Spinner />;
+  }
   return (
     <Box>
       <h1>게시물 목록</h1>
@@ -18,22 +34,28 @@ export function BoardList() {
           <Thead>
             <Tr>
               <Th>id</Th>
-              <Th>Title</Th>
+              <Th>title</Th>
               <Th>by</Th>
               <Th>at</Th>
             </Tr>
           </Thead>
+          <Tbody>
+            {boardList.map((board) => (
+              <Tr
+                _hover={{
+                  cursor: "pointer",
+                }}
+                key={board.id}
+                onClick={() => navigate("/board/" + board.id)}
+              >
+                <Td>{board.id}</Td>
+                <Td>{board.title}</Td>
+                <Td>{board.writer}</Td>
+                <Td>{board.inserted}</Td>
+              </Tr>
+            ))}
+          </Tbody>
         </Table>
-        {boardList || <Spinner />}
-        {boardList &&
-          boardList.map((board) => (
-            <Tr>
-              <Td>{board.id}</Td>
-              <Td>{board.title}</Td>
-              <Td>{board.writer}</Td>
-              <Td>{board.inserted}</Td>
-            </Tr>
-          ))}
       </Box>
     </Box>
   );
