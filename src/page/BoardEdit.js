ceb90b1e32f0179a0e6edaf985583e89f494decb
hoonil1 <phi0588@naver.com>
2023-11-10 09:42:08 +0900

    제목과 콘텐츠 변경하는 handle 만들기

--- src/page/BoardEdit.js
+++ src/page/BoardEdit.js
@@ -1,5 +1,7 @@
 import {
   Box,
+  Button,
+  Flex,
   FormControl,
   FormLabel,
   Input,
@@ -27,21 +29,43 @@ export function BoardEdit() {
     return <Spinner />;
   }
 
+  function titleChange(e) {
+    updateBoard((draft) => {
+      draft.title = e.target.value;
+    });
+  }
+
+  function contentChange(e) {
+    updateBoard((draft) => {
+      draft.content = e.target.value;
+    });
+  }
+
+  function writerChange(e) {
+    updateBoard((draft) => {
+      draft.writer = e.target.value;
+    });
+  }
+
   return (
     <Box>
       <h1>{id} 번 글 수정</h1>
       <FormControl>
         <FormLabel>제목</FormLabel>
-        <Input value={board.title} />
+        <Input value={board.title} onChange={titleChange} />
       </FormControl>
       <FormControl>
         <FormLabel>본문</FormLabel>
-        <Textarea value={board.content} />
+        <Textarea value={board.content} onChange={contentChange} />
       </FormControl>
       <FormControl>
         <FormLabel>작성자</FormLabel>
-        <Input value={board.writer} />
+        <Input value={board.writer} onChange={writerChange} />
       </FormControl>
+      <Flex gap={7}>
+        <Button>수정</Button>
+        <Button>삭제</Button>
+      </Flex>
     </Box>
   );
 }
