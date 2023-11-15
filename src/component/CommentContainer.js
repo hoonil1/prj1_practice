f0238b94d0182f7bff72822d5ed59d0ed31a2725
hoonil1 <phi0588@naver.com>
2023-11-15 11:23:35 +0900

    쌤꺼 -> 댓글작성

--- src/component/CommentContainer.js
+++ src/component/CommentContainer.js
@@ -1,6 +1,18 @@
-import { Box, Button, Input, Textarea } from "@chakra-ui/react";
-import axios from "axios";
+import {
+  Box,
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Flex,
+  Heading,
+  Stack,
+  StackDivider,
+  Text,
+  Textarea,
+} from "@chakra-ui/react";
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 function CommentForm({ boardId }) {
   const [comment, setComment] = useState("");
@@ -21,15 +33,39 @@ function CommentForm({ boardId }) {
 }
 
 function CommentList({ boardId }) {
-  const [commentList, setCommentList] = useState(null);
+  const [commentList, setCommentList] = useState([]);
+
   useEffect(() => {
     const params = new URLSearchParams();
     params.set("id", boardId);
+
     axios
       .get("/api/comment/list?" + params)
       .then((response) => setCommentList(response.data));
   }, []);
-  return <Box>댓글 리스트</Box>;
+
+  return (
+    <Card>
+      <CardHeader>
+        <Heading size="md">댓글 리스트</Heading>
+      </CardHeader>
+      <CardBody>
+        <Stack divider={<StackDivider />} spacing="4">
+          {commentList.map((comment) => (
+            <Box>
+              <Flex justifyContent="space-between">
+                <Heading size="xs">{comment.memberId}</Heading>
+                <Text fontSize="xs">{comment.inserted}</Text>
+              </Flex>
+              <Text sx={{ whiteSpace: "pre-wrap" }} pt="2" fontSize="sm">
+                {comment.comment}
+              </Text>
+            </Box>
+          ))}
+        </Stack>
+      </CardBody>
+    </Card>
+  );
 }
 
 export function CommentContainer({ boardId }) {
